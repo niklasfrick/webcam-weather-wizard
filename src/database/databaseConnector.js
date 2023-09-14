d4ac074ddc07fb9f94da1b56286f53e319940d23
niklasfrick <niklas@Niklas-MBP.fritz.box>
2023-09-14 20:03:53 +0200

    :sparkles: feat: added a simple database connector and setup jest tests for it

---
+++ src/database/databaseConnector.js
@@ -0,0 +1,36 @@
+// mongodbConnector.js
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+// Load MongoDB Atlas credentials from .env
+const { MONGODB_URI, MONGODB_DB } = process.env;
+
+let dbClient;
+
+async function connectToDatabase() {
+  if (!dbClient) {
+    try {
+      dbClient = new MongoClient(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      await dbClient.connect();
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    }
+  }
+
+  return dbClient.db(MONGODB_DB);
+}
+
+function closeDatabase() {
+  if (dbClient) {
+    dbClient.close();
+    console.log("Closed MongoDB connection");
+  }
+}
+
+module.exports = { connectToDatabase, closeDatabase };
---
+++ test/databaseConnector.test.js
@@ -0,0 +1,38 @@
+// mongodbConnector.test.js
+const { MongoClient } = require("mongodb");
+const {
+  connectToDatabase,
+  closeDatabase,
+} = require("../src/database/databaseConnector");
+
+describe("MongoDB Connector Tests", () => {
+  let db;
+
+  beforeAll(async () => {
+    db = await connectToDatabase();
+  });
+
+  afterAll(() => {
+    closeDatabase();
+  });
+
+  it("should connect to the MongoDB database", async () => {
+    expect(db).toBeDefined();
+  });
+
+  it("should find a document in the database", async () => {
+    const collection = db.collection("snapshot-data");
+    const query = { _id: "1" };
+
+    const foundDocument = await collection.findOne(query);
+    expect(foundDocument).toEqual({
+      _id: "1",
+      date_time: "2023-09-14T17:26:04Z",
+      temperature_celsius: 14.6,
+      wind_speed_kph: 1.4,
+      total_precipitation_mm: 2,
+      snow_coverage: 0,
+      detected_objects: ["tent", "animal", "tree", "mountain"],
+    });
+  });
+});
